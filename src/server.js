44dd2cd15a066e6b8559193d84e91b9a85ca5fb5
Felipe <felipe.carvalho.dm@gmail.com>
2025-04-18 18:16:30 -0300

    teste

---
+++ src/server.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(5, 4);
+console.log(result);
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+function sum(a: number, b: number){
+
+    return a + b
+}
+
+let result: number = sum(5, 4);
+
+console.log(result);
