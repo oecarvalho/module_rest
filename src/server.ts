7ce87d84b6312668360deef8d802ec4362f12d1b
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-04-19 16:59:47 -0300

    teste2

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ const PORT = 3333
 
 const app = express();
 
+app.get('/products', (request, response)=>{
+    response.send(`Produtos`)
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is running at ${PORT}`)
 });
